36c15f6964bdeb94a6aea74765a7b5a332ae4a1a
Rafael Pinho <faelpinho@users.noreply.github.com>
2023-10-12 08:32:01 -0300

    Delete unused file from bin.

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 cd bin
-rm eboot.bin meuhello meuhello.self.out meuhello.velf meuhello.vpk.out
 
 cmake .. && make
 
 cp meuhello.self eboot.bin
+
+rm meuhello meuhello.self meuhello.self.out meuhello.velf meuhello.vpk.out
 
 cp meuhello.vpk ~/.local/share/Vita3K/Vita3K/ux0/temp/meuhello.vpk
 
